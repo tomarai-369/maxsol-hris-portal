258b5597ad8abfd681cf12d4492817780ccf53aa
tomarai-369 <tom_arai@edamame-jp.com>
2026-01-08 00:06:12 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
-  env: {
-    KINTONE_DOMAIN: process.env.KINTONE_DOMAIN,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mscorp.com.ph',
+      },
+    ],
   },
 };
 
